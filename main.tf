f84b6c6df4d73f13e75e299426cb8701222f8faa
GyaanSingh555 <gyaneshsengar55555@gmail.com>
2026-04-29 15:41:29 +0530

    commited on 29-april-2026

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_storage_account" "st-account-name" {
   resource_group_name = azurerm_resource_group.rg-name
   location = azurerm_resource_group.rg-name.location
   account_tier = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GZRS"
 }
 
 
